a6817077c715984ef174a5710eb59323fb35c307
Elan <a932327192@gmail.com>
2024-12-07 23:18:10 -0500

    modify ami

--- main.tf
+++ main.tf
@@ -2,9 +2,13 @@ provider "aws" {
     region = "us-east-1"
 }
 
+data "aws_ssm_parameter" "amzn2_linux" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
+
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"
-    instance_type = "t2.micro"
+    ami           = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
+    instance_type = "t3.micro"
     count         = var.core_count
 
     tags = {
